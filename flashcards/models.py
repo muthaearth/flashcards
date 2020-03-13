491bd4a667a686323f9d23f03550579d4bcd15cf
cheryl <rook2q5@gmail.com>
2020-03-13 11:30:36 -0400

    fix FlashCard

--- flashcards/models.py
+++ flashcards/models.py
@@ -6,22 +6,56 @@ from django.dispatch import receiver
 from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.authtoken.models import Token
 from datetime import timedelta
-# from django.contrib.auth import get_user_model
-# from users.models import User
+from django.contrib.auth import get_user_model
+from users.models import User
 
 
-class Flashcard(models.Model):
+class Deck(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField(default='')
+
+    def __str__(self):
+        return self.name
+
+    def get_cards_num(self):
+        cards = FlashCard.objects.get_cards_to_study(
+            user=self.owner, deck_id=self.id, days=0)
+        return len(cards)
+
+
+class FlashCardManager(models.Manager):
+    def create_FlashCard(self, user, question, answer, deck_name):
+        try:
+            deck = Deck.objects.get(owner=user, name=deck_name)
+        except ObjectDoesNotExist:
+            deck = Deck(owner=user, name=deck_name)
+            deck.save()
+
+        self.create(owner=user, question=question, answer=answer,
+                    deck=deck)
+        return deck
+
+    def get_cards_to_study(self, user, deck_id, days):
+        ##import ipdb; ipdb.set_trace()
+        next_due_date = timezone.now() + timedelta(days=days)
+        cards = FlashCard.objects.filter(deck__id=deck_id, deck__owner=user,
+                                         next_due_date__lte=next_due_date)
+        return cards
+
+
+class FlashCard(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     deck = models.ForeignKey(Deck, on_delete=models.CASCADE)
-    question = models.TextField()
-    answer = models.TextField()
+    question = models.TextField(max_length=100, blank=True, null=True)
+    answer = models.TextField(max_length=100, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     last_shown_at = models.DateTimeField(auto_now_add=True)
     next_due_date = models.DateTimeField(default=timezone.now)
     difficulty_level = models.FloatField(default=2.5)
     consec_correct_answers = models.IntegerField(default=0)
 
-    objects = FlashcardManager()
+    objects = FlashCardManager()
 
     def __str__(self):
         return self.question
@@ -34,7 +68,7 @@ class Flashcard(models.Model):
 
         Returns:
             next_due_date,difficulty_level,consec_correct_answers (tuple) - information
-                to update Flashcard data
+                to update FlashCard data
         """
 
         correct = (rating >= 3)
@@ -62,41 +96,7 @@ class Flashcard(models.Model):
             self.difficulty_level = result[1]
             self.consec_correct_answers = result[2]
         # Call the "real" save() method.
-        super(Flashcard, self).save(*args, **kwargs)
-
-
-class Deck(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
-    description = models.TextField(default='')
-
-    def __str__(self):
-        return self.name
-
-    def get_cards_num(self):
-        cards = Flashcard.objects.get_cards_to_study(
-            user=self.owner, deck_id=self.id, days=0)
-        return len(cards)
-
-
-class FlashcardManager(models.Manager):
-    def create_flashcard(self, user, question, answer, deck_name):
-        try:
-            deck = Deck.objects.get(owner=user, name=deck_name)
-        except ObjectDoesNotExist:
-            deck = Deck(owner=user, name=deck_name)
-            deck.save()
-
-        self.create(owner=user, question=question, answer=answer,
-                    deck=deck)
-        return deck
-
-    def get_cards_to_study(self, user, deck_id, days):
-        ##import ipdb; ipdb.set_trace()
-        next_due_date = timezone.now() + timedelta(days=days)
-        cards = Flashcard.objects.filter(deck__id=deck_id, deck__owner=user,
-                                         next_due_date__lte=next_due_date)
-        return cards
+        super(FlashCard, self).save(*args, **kwargs)
 
 
 @receiver(post_save, sender=User)
