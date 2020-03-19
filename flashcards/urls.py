23e10a8c26d42144ffcc4f05b83807eebd850302
cheryl <rook2q5@gmail.com>
2020-03-19 18:45:34 -0400

    correct_score

--- flashcards/urls.py
+++ flashcards/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
          views.correct_answer, name='correct_answer'),
     path('<slug:deck_slug>/<int:pk>/incorrect/',
          views.incorrect_answer, name='incorrect_answer'),
-    path('<slug:deck_slug><int:pk>/correct_score/',
-         views.correct_score, name='correct_score')
+#     path('<slug:deck_slug><int:pk>/correct_score/',
+#          views.correct_score, name='correct_score'),
