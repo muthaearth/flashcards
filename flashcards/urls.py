9ca995dc1e13d90363b79ebccdd67eb15a768be8
cheryl <rook2q5@gmail.com>
2020-03-19 18:35:26 -0400

    test

--- flashcards/urls.py
+++ flashcards/urls.py
@@ -1,24 +1,12 @@
 from django.urls import include, path
 from . import views
-# from rest_framework import routers
 
 
 urlpatterns = [
-    # path('', include(router.urls)),
     path('', views.home, name="home"),
-    #     accounts logout
-    #     path('accounts/logout', include('registration.backends.simple.urls')),
     path('<slug:deck_slug>/<int:pk>/',
          views.flashcard_display, name='flashcard_display'),
     path('<slug:deck_slug>/<int:pk>/correct/',
          views.correct_answer, name='correct_answer'),
     path('<slug:deck_slug>/<int:pk>/incorrect/',
          views.incorrect_answer, name='incorrect_answer')
-
-    #     path('<slug:deck_slug>/<int:pk>/scorecard/',
-    #          views.incorrect_answer, name='scorecard')
-]
-
-# router = routers.DefaultRouter()
-# router.register('deck', views.DeckView)
-# router.register('flashards', views.FlashCardView)
