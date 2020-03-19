7fa0e6668bd90208d2133cacd7de9867fb65b0b2
cheryl <rook2q5@gmail.com>
2020-03-19 19:17:35 -0400

    fix urls.py

--- flashcards/urls.py
+++ flashcards/urls.py
@@ -4,11 +4,13 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+
     path('<slug:deck_slug>/<int:pk>/',
          views.flashcard_display, name='flashcard_display'),
     path('<slug:deck_slug>/<int:pk>/correct/',
          views.correct_answer, name='correct_answer'),
     path('<slug:deck_slug>/<int:pk>/incorrect/',
          views.incorrect_answer, name='incorrect_answer'),
-#     path('<slug:deck_slug><int:pk>/correct_score/',
-#          views.correct_score, name='correct_score'),
+    path('<slug:deck_slug>/<int:pk>/scorecard/',
+         views.incorrect_answer, name='scorecard')
+]
